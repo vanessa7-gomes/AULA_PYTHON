a8c6e8140befcdfa0824e00368268259a241ed7e
Vanessa Gomes <vanessa.ribeirog7@gmail.com>
2021-02-23 17:47:26 -0300

    Programa para digitar nome idade e quantos anos tera daqui 5 anos

---
+++ nome.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+idade = int(idade) + 5
+print("Seu nome é " + nome)
+print("Sua idade daqui a cinco anos será " + str (idade) )
